be4034592b2d54f4aaea5b4a4aa4330d54fb7e59
gokuldharan <gokul.dharan@mail.utoronto.ca>
2020-11-08 16:35:33 -0800

    more flexible simulate_games1 and unified guesser

---
+++ codenames/simulate_games1.py
@@ -0,0 +1,81 @@
+import time
+import json
+
+from game import Game
+from players.codemaster_w2vglovebert import AICodemaster as cm
+from players.guesser_w2vglovebert import AIGuesser as g_unif
+from players.guesser_with_state import AIGuesserWithState as g_with_state
+from players.guesser_random import AIGuesser as g_rand
+
+
+
+#Just change these for diff guesser types, embeddings
+CM_EMB = "BERT"     #BERT, GLOVE, or W2V
+G_EMB = "BERT"
+G_TYPE = "BASELINE" #BASELINE, WITH_STATE, or RANDOM (ALSO Q?)
+
+
+
+
+argToVec = {}
+argToKey = {"BERT":"bert_vecs", "GLOVE":"glove_vecs", "W2V" : "word_vectors"}
+typeToGuesser = {"BASELINE":g_unif, "WITH_STATE":g_with_state, "RANDOM":g_rand}
+
+class Simulation:
+    """Example of how to share vectors, pass kwargs, and call Game directly instead of by terminal"""
+
+    start_time = time.time()
+    argToVec["GLOVE"] = Game.load_glove_vecs("players/glove.6B.50d.txt")
+    print(f"{time.time() - start_time:.2f}s to load glove50d")
+
+    start_time = time.time()
+    argToVec["BERT"] = Game.load_bert_vecs("players/bert_embeddings.txt")
+    print(f"{time.time() - start_time:.2f}s to load BERT")
+
+    start_time = time.time()
+    argToVec["W2V"] = Game.load_w2v("players/GoogleNews-vectors-negative300.bin")
+    print(f"{time.time() - start_time:.2f}s to load w2v")
+
+    print("\nclearing results folder...\n")
+    Game.clear_results()
+
+    cm_vec = argToVec[CM_EMB]
+    cm_key = argToKey[CM_EMB]
+    g_vec = argToVec[G_EMB]
+    g_key = argToKey[G_EMB]
+
+    g = typeToGuesser[G_TYPE]
+
+    start_time = time.time()
+
+    num_games = 100
+    for seed in range(num_games):
+        game_name = str(CM_EMB) +"-"+ str(G_EMB) + "-" + str(G_TYPE) + str(seed)
+        print("starting " + game_name)
+        cm_kwargs = {cm_key: cm_vec}
+        g_kwargs = {g_key: g_vec}
+        Game(cm, g, seed=seed, do_print=False,  game_name=game_name, cm_kwargs=cm_kwargs, g_kwargs=g_kwargs).run()
+
+    # display the results (AVG NUM TUNRS, MIN NUM TURNS, WIN PERCENTAGE).
+    num_turns = 0
+    min_num_turns = 25
+    num_wins = 0
+    with open("results/bot_results_new_style.txt") as f:
+        for line in f.readlines():
+            game_json = json.loads(line.rstrip())
+            turns = game_json["total_turns"]
+            num_turns += turns
+            min_num_turns = min(turns, min_num_turns)
+
+            red_cards_guessed = game_json["R"]
+            if red_cards_guessed == 8:
+                num_wins += 1
+
+
+    avg_turns = 1.0 * num_turns / num_games
+    win_pct = 1.0 * num_wins / num_games
+    print(f"avg_turns={avg_turns}, min_turns={min_num_turns}, win_pct={win_pct}")
+
+
+if __name__ == "__main__":
+    Simulation()
